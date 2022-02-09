ecf3378a424103208f78ca828404227acdd2ab46
ElizabethChillcce <eli.chillcce.08@gmail.com>
2022-02-08 20:51:00 -0500

    Iniciando con js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+//alert("¡Hola! bienvenido(a) a Sumaq Garden");
+
+
+function mostrarPagina(){
+    document.getElementById("paginaPrincipal").style.display="none";
+    document.getElementById("date").style.display="block";
+}
+
+let buttonUno= document.getElementById("buttonBuy");
+buttonUno.addEventListener("click",mostrarPagina);
